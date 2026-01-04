f2075c7ee5223ca9202e82eb99d4db3f1514c42f
Þórarinn Hjálmarsson <thorarinnhjalmarsson@gmail.com>
2026-01-04 22:10:09 +0000

    fix: Allow Google profile images in Next.js image config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
