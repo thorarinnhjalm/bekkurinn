f45127a4b6eaecc7025c8996b87d5c2176e2ac86
Þórarinn Hjálmarsson <thorarinnhjalmarsson@gmail.com>
2026-01-24 11:45:27 +0000

    feat: add support for Tagalog (tl), Ukrainian (uk), and Vietnamese (vi)

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,4 +1,4 @@
-export const locales = ['en', 'is', 'pl', 'es', 'lt'] as const;
+export const locales = ['en', 'is', 'pl', 'es', 'lt', 'tl', 'uk', 'vi'] as const;
 export type Locale = (typeof locales)[number];
 
 export const defaultLocale: Locale = 'is';
